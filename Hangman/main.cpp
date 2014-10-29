5f70fccfdb89c65254196510ef28425a415750e2
arriettyc <cuix877@hotmail.com>
2014-10-29 16:44:00 +0800

    Initial Commit

---
+++ Hangman/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hangman
+//
+//  Created by xin cui on 14/10/29.
+//  Copyright (c) 2014年 xin cui. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
